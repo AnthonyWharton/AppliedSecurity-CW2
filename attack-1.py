db680e74127c5507f806c654690bf4674dedb97a
Anthony Wharton <a.wharton.2015@bristol.ac.uk>
2018-03-03 00:52:43 +0000

    Fix: Stage 3, Remove: Decimal library
    	- Cleaned up print-outs
    	- Cleaned up code

--- attack-1.py
+++ attack-1.py
@@ -1,11 +1,16 @@
 import sys, subprocess, math
-from decimal import Decimal
 
 # Reference 1:
 # Manger, J., 2001, August. A chosen ciphertext attack on RSA optimal asymmetric
 # encryption padding (OAEP) as standardized in PKCS# 1 v2. 0. In Annual
 # International Cryptology Conference (pp. 230-238). Springer, Berlin,
 # Heidelberg.
+
+def centre_trunc_string(ln, s):
+	if ln % 2 == 0:
+		return s[:(ln/2)-3] + " .... " + s[-(ln/2)+2:] if len(s) > ln else s
+	else:
+		return s[:(ln/2)-2] + " ... "  + s[-(ln/2)+2:] if len(s) > ln else s
 
 ################################################################################
 # Reads in the configuration for the attack from a given file.
@@ -18,13 +23,13 @@ def read_config(path):
 	c = config_file.readline()[:-1]
 	config_file.close()
 
-	print "================================================================"
+	print "=" * 80
 	print "Read in the following values from Config File:"
-	print "          RSA Modulus: " + N
-	print "  RSA Public Exponent: " + e
-	print "     RSAES-OAEP Label: " + l
-	print "RSAES-OAEP Ciphertext: " + c
-	print "================================================================"
+	print "          RSA Modulus: " + centre_trunc_string(57, N)
+	print "  RSA Public Exponent: " + centre_trunc_string(57, e)
+	print "     RSAES-OAEP Label: " + centre_trunc_string(57, l)
+	print "RSAES-OAEP Ciphertext: " + centre_trunc_string(57, c)
+	print "=" * 80
 	return N, e, l, c
 
 ################################################################################
@@ -35,13 +40,13 @@ def convert_config(N_str, e_str, l_str, c_str):
 	e = int(e_str, 16)  # RSA Public Exponent
 	l = int(l_str, 16)  # RSAES-OAEP label
 	c = int(c_str, 16)  # RSAES-OAEP ciphertext
-	print "================================================================"
+	print "=" * 80
 	print "Converted the following values from Config File:"
-	print "          RSA Modulus: " + str(N)
-	print "  RSA Public Exponent: " + str(e)
-	print "     RSAES-OAEP Label: " + str(l)
-	print "RSAES-OAEP Ciphertext: " + str(c)
-	print "================================================================"
+	print "          RSA Modulus: " + centre_trunc_string(57, str(N))
+	print "  RSA Public Exponent: " + centre_trunc_string(57, str(e))
+	print "     RSAES-OAEP Label: " + centre_trunc_string(57, str(l))
+	print "RSAES-OAEP Ciphertext: " + centre_trunc_string(57, str(c))
+	print "=" * 80
 	return N, e, l, c
 
 ################################################################################
@@ -52,88 +57,87 @@ def create_challenge(m, N, e, c=1):
 	return _challenge_format.format(t)
 
 ################################################################################
+# Helper functions for integer division with floor and ceiling rounding
+def int_div_f(x, y):
+	return divmod(x, y)[0]
+
+def int_div_c(x, y):
+	q, m = divmod(x, y)
+	if m == 0:
+		return q
+	else:
+		return q + 1
+
+################################################################################
 # Feeds a given label and ciphertext to the target, and returns the status code
-def interact(target, label, ciphertext):
+_challenge_count = 0
+def challenge(target, label, ciphertext):
 	target.stdin.write(label      + "\n")
 	target.stdin.write(ciphertext + "\n")
 	target.stdin.flush()
+	global _challenge_count
+	_challenge_count = _challenge_count + 1
 	return int(target.stdout.readline().strip())
 
 ################################################################################
 # Performs Step 1 from the attack in Reference 1
 def step1(target, N, e, l, c):
 	f1 = 1
-	status = -1
+	st = -1
 
-	while status != 1:
+	while st != 1:
 		f1 *= 2
-		# challenge is in [0, 2B]
-		challenge = create_challenge(f1, N, e, c)
-		status = interact(target, l, challenge)
+		ch = create_challenge(f1, N, e, c)
+		st = challenge(target, l, ch)
 
-	# f1 is in [B, 2B]
 	return f1
 
 ################################################################################
 # Performs Step 2 from the attack in Reference 1
 def step2(target, N, e, l, c, f1, B):
-	f2 = int(math.floor(Decimal(N + B) / B) * (f1/2))
-	challenge = create_challenge(f2, N, e, c)
-	print challenge
-	status = interact(target, l, challenge)
+	ft = int_div_f(f1, 2)
+	f2 = int_div_f(N + B, B) * ft
+	ch = create_challenge(f2, N, e, c)
+	st = challenge(target, l, ch)
 
-	count = 0
-	while status == 1:# or status == 6: # TODO, Should we allow 6?
-		f2 = f2 + (f1/2)
-		challenge = create_challenge(f2, N, e, c)
-		status = interact(target, l, challenge)
-		count += 1
+	while st == 1:
+		f2 = f2 + ft
+		ch = create_challenge(f2, N, e, c)
+		st = challenge(target, l, ch)
 
-	print "Status: " + str(status) + ", iters: " + str(count) + " (of " + str(N/B) + ")"
-	# f2 is in [N, N+B]
 	return f2
 
 ################################################################################
 # Performs Step 3 from the attack in Reference 1
 def step3(target, N, e, l, c, f2, B):
-	m_min = Decimal(math.ceil (Decimal(N)     / f2))
-	m_max = Decimal(math.floor(Decimal(N + B) / f2))
+	m_min = int_div_c(N,     f2)
+	m_max = int_div_f(N + B, f2)
 	f3 = 0
 
-	print "min: " + str(m_min) + "\nmax: " + str(m_max)
-
 	while m_min != m_max:
-		print str(int(m_max-m_min))
-		f_tmp = Decimal(math.floor((2 * B)         / (m_max - m_min)))
-		i     = Decimal(math.floor((f_tmp * m_min) / N))
-		f3    = Decimal(math.ceil ((i * N)         /  m_min))
-		b     = Decimal(i * N + B)
+		f_tmp = int_div_f(        2 * B, m_max - m_min)
+		i     = int_div_f(f_tmp * m_min, N)
+		f3    = int_div_c(        i * N, m_min)
+		b     = i * N + B
 
-		challenge = create_challenge(int(f3), N, e, c)
-		status    = interact(target, l, challenge)
+		ch = create_challenge(int(f3), N, e, c)
+		st = challenge(target, l, ch)
 
-		if status == 0:
-			print "Plaintext found"
-			break
-		elif status == 1:# or status == 6:
-			# print ">= B"
-			# print "before min: " + str(int(m_min))
-			m_min = Decimal(math.ceil (b / f3))
-			# print "after min: " + str(int(m_min))
+		if st == 1:# or status == 6:
+			m_min = int_div_c(b, f3)
 		else:
-			# print "< B"
-			# print "before max: " + str(int(m_max))
-			m_max = Decimal(math.floor(b / f3))
-			# print "after max: " + str(int(m_max))
+			m_max = int_div_f(b, f3)
 
-	print " m_min: " + str(m_min)
-	print " m_max: " + str(m_max)
-	print "Status: " + str(status)
+	return f3, m_max
 
-	t = pow(int(m_max), e, N)
-	print "STATUS CODE: " + str(interact(target, l, hex(t).upper()[2:]))
-
-	return f3
+################################################################################
+# Runs a step of the attack with nice printing
+def run_step(n, f, args):
+	sys.stdout.write("Starting Step " + str(n) + " ... ")
+	r = f(*args)
+	sys.stdout.write("DONE (" + str(_challenge_count).rjust(4) +
+	                 " challenges made)\n")
+	return r
 
 ################################################################################
 # Performs the attack
@@ -141,7 +145,7 @@ def attack(target, config_path):
 	Ns, es, ls, cs = read_config(config_path)
 	Ni, ei, li, ci = convert_config(Ns, es, ls, cs)
 
-	# Calculate the formatting 
+	# Compute the formatting string for challenges
 	global _challenge_format
 	_challenge_format = "{0:0" + str(len(Ns)) + "X}"
 
@@ -149,21 +153,18 @@ def attack(target, config_path):
 	k = int(math.ceil(math.log(Ni, 256)))
 	B = 2 ** (8*(k-1))
 
-	print "Starting Step 1"
-	f1 = step1(target, Ni, ei, ls, ci)
-	print "f1: " + str(f1)
+	# Run Attack Steps
+	f1        = run_step(1, step1, (target, Ni, ei, ls, ci))
+	f2        = run_step(2, step2, (target, Ni, ei, ls, ci, f1, B))
+	f3, m_max = run_step(3, step3, (target, Ni, ei, ls, ci, f2, B))
 
-	print "Starting Step 2"
-	f2 = step2(target, Ni, ei, ls, ci, f1, B)
-	print "f2: " + str(f2)
-
-	print "Starting Step 3"
-	f3 = step3(target, Ni, ei, ls, ci, f2, B)
-	# print "f3: " + str(f3)
-	# print " B: " + str(B)
+	return "memes"
 
 ################################################################################
+# Main
 def main():
+	version_warning()
+
 	# Get the file locations for the "e-commerce server" (target) and public
 	# configuration parameters
 	server_path = sys.argv[1]
@@ -174,8 +175,25 @@ def main():
 	                          stdout=subprocess.PIPE,
 	                          stdin=subprocess.PIPE)
 
-	# Execute a function representing the attacker.
-	attack(target, config_path)
+	# Execute the attack
+	m = attack(target, config_path)
+
+	version_warning()
+	print "Extracted Material: " + str(m)
+	print "Interactions with Target: " + str(_challenge_count)
+
+def version_warning():
+	if sys.version_info[:3] != (2,7,5):
+		print "!" * 80
+		print "!!!!" + " " * 13 + \
+		      "WARNING, RUNNING ON UNTESTED VERSION OF PYTHON" + \
+		      " " * 13 + "!!!!"
+		print "!!!!" + " There could be untested behaviour, thus it is " + \
+		      "recommended you run with " + "!" * 4
+		print "!!!!" + " " * 13 + \
+		      "Python 2.7.5 or compatible for optimum results." + \
+		      " " * 12 + "!!!!"
+		print "!" * 80
 
 if __name__ == "__main__":
 	main()
